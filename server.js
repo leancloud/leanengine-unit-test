3cb77f547c1901f9bd7ea055237fa99aefcb6a65
jysperm <jysperm@gmail.com>
2017-01-11 15:05:56 +0800

    install-dev-dependencies

---
+++ server.js
@@ -0,0 +1,11 @@
+require('http').createServer(function(req, res) {
+  if (req.url == '/')
+    res.statusCode = 200;
+  else
+    res.statusCode = 404;
+
+  res.end();
+}).listen(3000);
+
+require('lodash');
+require('mocha');
