e7ee953ff13c8b8dc847322060a1ff0b4db9020c
wchen <sdjcw13@gmail.com>
2017-03-14 17:33:44 +0800

    使用 npm shrinkwrap 固定某个组件的版本

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
+var assert = require('assert')
+
+assert.equal('2.1.12', require('mime-types/package.json').version)
 
 var app = express();
 
