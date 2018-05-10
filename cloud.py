c2034b38d1a5f777c683acb668ef41c939bc8825
jysperm <jysperm@gmail.com>
2018-05-10 17:08:22 +0800

    Python 2.7

---
+++ cloud.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from leancloud import Engine
+from leancloud import LeanEngineError
+
+engine = Engine()
---
+++ wsgi.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from gevent import monkey
+monkey.patch_all()
+
+import os
+
+import leancloud
+
+from app import app
+from cloud import engine
+
+APP_ID = os.environ['LEANCLOUD_APP_ID']
+APP_KEY = os.environ['LEANCLOUD_APP_KEY']
+MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
+PORT = int(os.environ['LEANCLOUD_APP_PORT'])
+
+leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
+leancloud.use_master_key(False)
+
+app = engine.wrap(app)
+application = app
+
+if __name__ == '__main__':
+    # 只在本地开发环境执行的代码
+    from gevent.pywsgi import WSGIServer
+    from geventwebsocket.handler import WebSocketHandler
+    from werkzeug.serving import run_with_reloader
+    from werkzeug.debug import DebuggedApplication
+
+    @run_with_reloader
+    def run():
+        global application
+        app.debug = True
+        application = DebuggedApplication(application, evalex=True)
+        server = WSGIServer(('localhost', PORT), application, handler_class=WebSocketHandler)
+        server.serve_forever()
+
+    run()
