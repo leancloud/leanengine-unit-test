f04d9b5f91dabae76a8132de53b294a822d378c5
jysperm <jysperm@gmail.com>
2022-07-21 20:02:28 +0800

    factorio

--- main.go
+++ main.go
@@ -1,96 +1,13 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"net"
+	"io/ioutil"
 	"net/http"
-	"time"
+	"os"
+	"os/exec"
 )
-
-// maxBufferSize specifies the size of the buffers that
-// are used to temporarily hold data from the UDP packets
-// that we receive.
-const maxBufferSize = 1024
-
-// udpEchoServer wraps all the UDP echo udpEchoServer functionality.
-// ps.: the udpEchoServer is capable of answering to a single
-// client at a time.
-func udpEchoServer(ctx context.Context, address string) (err error) {
-	// ListenPacket provides us a wrapper around ListenUDP so that
-	// we don't need to call `net.ResolveUDPAddr` and then subsequentially
-	// perform a `ListenUDP` with the UDP address.
-	//
-	// The returned value (PacketConn) is pretty much the same as the one
-	// from ListenUDP (UDPConn) - the only difference is that `Packet*`
-	// methods and interfaces are more broad, also covering `ip`.
-	pc, err := net.ListenPacket("udp", address)
-	if err != nil {
-		return
-	}
-
-	// `Close`ing the packet "connection" means cleaning the data structures
-	// allocated for holding information about the listening socket.
-	defer pc.Close()
-
-	doneChan := make(chan error, 1)
-	buffer := make([]byte, maxBufferSize)
-
-	// Given that waiting for packets to arrive is blocking by nature and we want
-	// to be able of canceling such action if desired, we do that in a separate
-	// go routine.
-	go func() {
-		for {
-			// By reading from the connection into the buffer, we block until there's
-			// new content in the socket that we're listening for new packets.
-			//
-			// Whenever new packets arrive, `buffer` gets filled and we can continue
-			// the execution.
-			//
-			// note.: `buffer` is not being reset between runs.
-			//	  It's expected that only `n` reads are read from it whenever
-			//	  inspecting its contents.
-			n, addr, err := pc.ReadFrom(buffer)
-			if err != nil {
-				doneChan <- err
-				return
-			}
-
-			fmt.Printf("packet-received: bytes=%d from=%s\n",
-				n, addr.String())
-
-			// Setting a deadline for the `write` operation allows us to not block
-			// for longer than a specific timeout.
-			//
-			// In the case of a write operation, that'd mean waiting for the send
-			// queue to be freed enough so that we are able to proceed.
-			deadline := time.Now().Add(time.Minute)
-			err = pc.SetWriteDeadline(deadline)
-			if err != nil {
-				doneChan <- err
-				return
-			}
-
-			// Write the packet's contents back to the client.
-			n, err = pc.WriteTo(buffer[:n], addr)
-			if err != nil {
-				doneChan <- err
-				return
-			}
-
-			fmt.Printf("packet-written: bytes=%d to=%s\n", n, addr.String())
-		}
-	}()
-
-	select {
-	case <-ctx.Done():
-		fmt.Println("cancelled")
-		err = ctx.Err()
-	case err = <-doneChan:
-	}
-
-	return
-}
 
 func httpServer() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -100,7 +17,45 @@ func httpServer() {
 	http.ListenAndServe(":3000", nil)
 }
 
+func factorio() {
+	if err := os.Chmod("./bin/x64/factorio", 0755); err != nil {
+		fmt.Println(err)
+	}
+
+	saveName, exists := os.LookupEnv("SAVE_NAME")
+
+	if !exists {
+		saveName = "big-mesh"
+	}
+
+	saveFile := "../cache/saves/" + saveName + ".zip"
+
+	if _, err := os.Stat(saveFile); errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll("../cache/saves", os.ModePerm)
+
+		bytesRead, err := ioutil.ReadFile("./saves/" + saveName + ".zip")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = ioutil.WriteFile(saveFile, bytesRead, 0644)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	cmd := exec.Command("./bin/x64/factorio", "--start-server", saveFile, "--port", "4000")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Println(cmd.Run())
+}
+
 func main() {
-	go udpEchoServer(context.Background(), ":4000")
+	go factorio()
+
 	httpServer()
 }
