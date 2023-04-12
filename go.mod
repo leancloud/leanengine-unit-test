ee16925e08815b423e0e66a3ad788dc48ae97de5
jysperm <jysperm@gmail.com>
2023-04-12 16:02:17 +0800

    :card_file_box: Sync saves with S3

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module main
 
 go 1.17
+
+require github.com/fsnotify/fsnotify v1.6.0
+
+require golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func httpServer() {
@@ -17,45 +20,84 @@ func httpServer() {
 	http.ListenAndServe(":3000", nil)
 }
 
-func factorio() {
-	if err := os.Chmod("./bin/x64/factorio", 0755); err != nil {
-		fmt.Println(err)
-	}
-
+func main() {
 	saveName, exists := os.LookupEnv("SAVE_NAME")
+	rcloneRemote := os.Getenv("RCLONE_REMOTE")
 
 	if !exists {
-		saveName = "big-mesh"
+		saveName = "first-game"
 	}
 
-	saveFile := "../cache/saves/" + saveName + ".zip"
+	saveFile := "./saves/" + saveName + ".zip"
+
+	createNewSave := func() {
+		stdout, err := exec.Command("./bin/x64/factorio", "--create", saveFile).Output()
+
+		fmt.Println(stdout)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+
+	startFactorio := func() {
+		cmd := exec.Command("./bin/x64/factorio", "--start-server", saveFile, "--port", "34197")
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		fmt.Println(cmd.Run())
+	}
+
+	startRcloneSync := func() {
+		watcher, err := fsnotify.NewWatcher()
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		defer watcher.Close()
+
+		if err := watcher.Add(filepath.Dir(saveFile)); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		for {
+			select {
+			case e := <-watcher.Events:
+				if !strings.HasSuffix(e.Name, ".tmp.zip") && !strings.HasSuffix(e.Name, ".bak.zip") {
+					fmt.Println("Uploading", e.Name, "to S3 ...")
+
+					cmd := exec.Command("./rclone", "copy", filepath.Dir(saveFile), rcloneRemote)
+
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+
+					fmt.Println(cmd.Run())
+				}
+
+			case err := <-watcher.Errors:
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+	}
+
+	fmt.Println("Downloading", saveFile, "from S3 ...")
+	stdout, err := exec.Command("./rclone", "copy", fmt.Sprint(rcloneRemote, "/", saveName, ".zip"), filepath.Dir(saveFile)).Output()
+
+	fmt.Println(stdout)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 
 	if _, err := os.Stat(saveFile); errors.Is(err, os.ErrNotExist) {
-		err := os.MkdirAll("../cache/saves", os.ModePerm)
-
-		bytesRead, err := ioutil.ReadFile("./saves/" + saveName + ".zip")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = ioutil.WriteFile(saveFile, bytesRead, 0644)
-
-		if err != nil {
-			fmt.Println(err)
-		}
+		fmt.Println("Creating new sav ", saveFile, "...")
+		createNewSave()
 	}
 
-	cmd := exec.Command("./bin/x64/factorio", "--start-server", saveFile, "--port", "4000")
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	fmt.Println(cmd.Run())
-}
-
-func main() {
-	go factorio()
+	go startFactorio()
+	go startRcloneSync()
 
 	httpServer()
 }
